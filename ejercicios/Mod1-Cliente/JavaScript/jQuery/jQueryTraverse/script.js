49d7426ddd7ea763bb2e252071c211899d84d999
Sylvia Llorente <sylviall81@gmail.com>
2024-02-20 20:30:25 +0100

    ejercicio jquery traverse finished

--- ejercicios/Mod1-Cliente/JavaScript/jQuery/jQueryTraverse/script.js
+++ ejercicios/Mod1-Cliente/JavaScript/jQuery/jQueryTraverse/script.js
@@ -1,4 +1,3 @@
-
 // $(document).ready(function(){
 
 //     // Seleccionar el div abuelo
@@ -34,19 +33,38 @@
 // })
 
 
-$(function(){
-    let main = $('#unico').children('div')
-    console.log(main[0])
-    main.css({'background-color':'black',
-                'width':'80%',
-            'height':'80%'})
-    let child = main.children();
-    child.css('background-color','#CCC')
-    let hermanos = child.children()
-    hermanos.css({'background-color':'red',
-    'width':'20%',
-    'border' : '1px solid navy',
-    'display' : 'inline-block',
-    'margin' : '20px',
-    'height':'20%'})
+$(function () {
+    let grandPaDiv = $('#unico').children('div')
+    console.log(grandPaDiv[0])
+    grandPaDiv.css({
+        'background-color': 'black',
+        'border': '1px solid blue',
+        'display': 'flex',
+        'align-items': 'center',
+        'justify-content': 'center',
+        'width': '80vw',
+        'height': '95vh',
+        'display': 'flex'
+
+    })
+
+    let dadDiv = grandPaDiv.children();
+    dadDiv.css({
+        'background-color': 'gray',
+        'width': '70%',
+        'height': '75%',
+        'border': '1px solid white',
+        'display': 'flex',
+        'align-items': 'center',
+        'justify-content': 'center'
+    })
+    let childDiv = dadDiv.children()
+    childDiv.css({
+        'background-color': 'red',
+        'width': '30%',
+        'border': '1px solid green',
+        'display': 'inline-block',
+        'margin': '20px',
+        'height': '40%'
+    })
 })
