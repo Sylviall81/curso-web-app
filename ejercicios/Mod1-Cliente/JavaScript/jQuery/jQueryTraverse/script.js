ba16c454d617c74aa4adda312b85879369ad7820
Sylvia Llorente <sylviall81@gmail.com>
2024-02-20 16:14:02 +0100

    traverse

--- ejercicios/Mod1-Cliente/JavaScript/jQuery/jQueryTraverse/script.js
+++ ejercicios/Mod1-Cliente/JavaScript/jQuery/jQueryTraverse/script.js
@@ -1,10 +1,34 @@
 
+$(document).ready(function(){
 
+    // Seleccionar el div abuelo
+    let children= $('#inner').children('div');
+    console.log('children',children);
+    let grandPaDiv = children[0];
+    console.log('grandPaDiv', grandPaDiv);
 
-let children = $('.inner').children()
-console.log(children)  
+    // Aplicar estilo al div abuelo
+    grandPaDiv.addClass('grid-grandpa');
+    grandPaDiv.css({
+                    'background-color': 'lightgray',
+                    'border' : '1px solid navy',
+                    'width': '75%'
+                    });
 
-let grandPaDiv = children[0]
-console.log(grandPaDiv);
+    // Seleccionar el div padre
+    let fatherDiv = grandPaDiv.children('div')
+    // Aplicar estilo al div padre
+    fatherDiv.css('border', '1px solid black');
 
-grandPaDiv.css('background-color','lightgray')
+    // Seleccionar los hijos (divs hijos)
+    let childDivs = fatherDiv.children();
+    // Aplicar estilo a los hijos (divs hijos)
+    childDivs.each(function(index) {
+        $(this).css('background-color', 'lightblue');
+        // Añadir un borde rojo al primer hijo (child1)
+        if (index === 0) {
+            $(this).css('border', '1px solid red');
+        }
+    });
+
+})
