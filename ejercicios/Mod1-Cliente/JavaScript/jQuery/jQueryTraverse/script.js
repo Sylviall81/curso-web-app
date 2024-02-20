78be5436e5daffc44387e6380adc9a156b23bd9d
Sylvia Llorente <sylviall81@gmail.com>
2024-02-20 16:32:58 +0100

    traverse a medias

--- ejercicios/Mod1-Cliente/JavaScript/jQuery/jQueryTraverse/script.js
+++ ejercicios/Mod1-Cliente/JavaScript/jQuery/jQueryTraverse/script.js
@@ -1,34 +1,52 @@
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    // Seleccionar el div abuelo
-    let children= $('#inner').children('div');
-    console.log('children',children);
-    let grandPaDiv = children[0];
-    console.log('grandPaDiv', grandPaDiv);
+//     // Seleccionar el div abuelo
+//     let children= $('#inner').children('div');
+//     console.log('children',children);
+//     let grandPaDiv = children[0];
+//     console.log('grandPaDiv', grandPaDiv);
 
-    // Aplicar estilo al div abuelo
-    grandPaDiv.addClass('grid-grandpa');
-    grandPaDiv.css({
-                    'background-color': 'lightgray',
-                    'border' : '1px solid navy',
-                    'width': '75%'
-                    });
+//     // Aplicar estilo al div abuelo
+//     grandPaDiv.addClass('grid-grandpa');
+//     grandPaDiv.css({
+//                     'background-color': 'lightgray',
+//                     'border' : '1px solid navy',
+//                     'width': '75%'
+//                     });
 
-    // Seleccionar el div padre
-    let fatherDiv = grandPaDiv.children('div')
-    // Aplicar estilo al div padre
-    fatherDiv.css('border', '1px solid black');
+//     // Seleccionar el div padre
+//     let fatherDiv = grandPaDiv.children('div')
+//     // Aplicar estilo al div padre
+//     fatherDiv.css('border', '1px solid black');
 
-    // Seleccionar los hijos (divs hijos)
-    let childDivs = fatherDiv.children();
-    // Aplicar estilo a los hijos (divs hijos)
-    childDivs.each(function(index) {
-        $(this).css('background-color', 'lightblue');
-        // Añadir un borde rojo al primer hijo (child1)
-        if (index === 0) {
-            $(this).css('border', '1px solid red');
-        }
-    });
+//     // Seleccionar los hijos (divs hijos)
+//     let childDivs = fatherDiv.children();
+//     // Aplicar estilo a los hijos (divs hijos)
+//     childDivs.each(function(index) {
+//         $(this).css('background-color', 'lightblue');
+//         // Añadir un borde rojo al primer hijo (child1)
+//         if (index === 0) {
+//             $(this).css('border', '1px solid red');
+//         }
+//     });
 
+// })
+
+
+$(function(){
+    let main = $('#unico').children('div')
+    console.log(main[0])
+    main.css({'background-color':'black',
+                'width':'80%',
+            'height':'80%'})
+    let child = main.children();
+    child.css('background-color','#CCC')
+    let hermanos = child.children()
+    hermanos.css({'background-color':'red',
+    'width':'20%',
+    'border' : '1px solid navy',
+    'display' : 'inline-block',
+    'margin' : '20px',
+    'height':'20%'})
 })
