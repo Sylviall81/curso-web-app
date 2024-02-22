9cd1fa9b0ad9a9c40e2d8e59a134a5c7d20d70d1
Sylvia Llorente <sylviall81@gmail.com>
2024-02-22 18:15:54 +0100

    listas

--- ejercicios/Mod1-Cliente/JavaScript/PreExam/script.js
+++ ejercicios/Mod1-Cliente/JavaScript/PreExam/script.js
@@ -1,56 +1,100 @@
 const arrayItems = [];
 
 
+function main(num) {
 
-
-
-
-
-
-
-function addItem() {
-
-    let userItem = document.getElementById('user-item').value;
+    let userItem = document.getElementById('user-item').value.toLowerCase();
     console.log(userItem);
-    arrayItems.push(userItem.toLowerCase());
+    
+
+
+    switch (num) {
+
+        //Añadir Item
+        case 1:
+
+        if (userItem === "" ){
+            alert("no pudes insertar un elemento vacio en la lista")
+        } else {
+            addItem(userItem);
+            clearInput('user-item')}
+
+            break;
+
+            //Borrar Item
+        case 2:
+
+        if (userItem === "" ){
+            alert("escribe un elemento para poder borrarlo")
+        } else {
+            eraseItem(userItem);
+
+        }
+
+            break;
+
+        default:
+            pickItem();
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function addItem(item) {
+
+    arrayItems.push(item);
     console.log(arrayItems);
 
     let fullList = document.getElementById('ordered-list');
     let listItem = document.createElement('li');
-    listItem.innerHTML = userItem;
+    listItem.innerHTML = item;
+    //listItem.innerHTML = arrayItems[arrayItems.length-1]
     fullList.appendChild(listItem);
 
-    clearInput('user-item')
+
 
 }
 
 
-function eraseItem() {
+function eraseItem(item) {
 
-    let userItem = document.getElementById('user-item').value
-    let itemIndex = arrayItems.indexOf(userItem.toLowerCase());
+    
+
+    let itemIndex = arrayItems.indexOf(item);
     console.log(itemIndex);
 
     if (itemIndex !== -1) {
 
-        let deletConfirmation = confirm(`Estas a punto de eliminar el elemento ${userItem} de la lista ¿Estas segur@ que quieres continuar?`)
-        
+        let deletConfirmation = confirm(`Estas a punto de eliminar el elemento ${item} de la lista ¿Estas segur@ que quieres continuar?`)
+
         if (deletConfirmation) {
             let liElement = document.getElementsByTagName('li')[itemIndex]; // Obtener el elemento <li> que queremos eliminar
             liElement.remove(); // Eliminar el elemento del DOM
             arrayItems.splice(itemIndex, 1);
             console.log(arrayItems)
             clearInput('user-item')
-        } else{
+        } else {
             clearInput('user-item')
         }
     } else {
-        let addNew = confirm(`El elemento ${userItem} no existe en la lista.¿Deseas añadirlo?`);
+        let addNew = confirm(`El elemento ${item} no existe en la lista.¿Deseas añadirlo?`);
 
         if (addNew) {
-            addItem();
+            addItem(item);
             console.log(arrayItems);
-        } else{
+            clearInput('user-item')
+        } else {
             clearInput('user-item')
         }
 
@@ -58,22 +102,24 @@ function eraseItem() {
 
 }
 
-function pickItem(){
+function pickItem() {
 
     let userNumber = Number(document.getElementById('user-number').value)
 
-    if( userNumber <= 0 || userNumber > arrayItems.length){
-        
-        if(arrayItems.length == 0){
+    if (userNumber <= 0 || userNumber > arrayItems.length) {
+
+        if (arrayItems.length == 0) {
             alert("Por favor, añade elementos a la lista")
         } else {
 
-        alert("Lo sentimos ese número no se encuentra en la lista, intenta de nuevo. Escoje un número del 1 al "+arrayItems.length)
+            alert("Lo sentimos ese número no se encuentra en la lista, intenta de nuevo. Escoje un número del 1 al " + arrayItems.length)
+            clearInput('user-number')
+       
         }
 
     } else {
 
-        document.getElementById('respuesta').innerHTML = 'has escogido:'+ arrayItems[userNumber-1]
+        document.getElementById('respuesta').innerHTML = 'has escogido:' + arrayItems[userNumber - 1]
         clearInput('user-number')
     }
 
